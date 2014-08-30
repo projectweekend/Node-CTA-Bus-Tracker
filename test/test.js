6c1014c77b4a47435f55ffa77e343f36b704b872
Brian Hines <brian@projectweekend.net>
2014-08-30 14:14:36 -0500

    Continue working on tests

--- test/test.js
+++ test/test.js
@@ -51,57 +51,44 @@ describe( "vehiclesByRoute", function () {
 } );
 
 
-// describe( "vehiclesById", function () {
-//     it( "calls the correct CTA URL", function ( done ) {
-//         busTracker.vehiclesById( [ "5555", "5556" ], function ( err, data ) {
-//             if ( err ) {
-//                 return done( err );
-//             }
-
-//             var apiUrl = "http://www.ctabustracker.com/bustime/api/v1/getvehicles?key=";
-//             apiUrl = apiUrl + apiKey;
-//             apiUrl = apiUrl + "&vid=5555,5556";
-//             expect( request.get.calledWith( apiUrl ) ).to.equal( true );
-
-//             done();
-//         } );
-//     } );
-// } );
-
-
-// describe( "vehiclesByRoute", function () {
-//     it( "calls the correct CTA URL", function ( done ) {
-//         busTracker.vehiclesByRoute( [ "8", "56" ], function ( err, data ) {
-//             if ( err ) {
-//                 return done( err );
-//             }
-
-//             var apiUrl = "http://www.ctabustracker.com/bustime/api/v1/getvehicles?key=";
-//             apiUrl = apiUrl + apiKey;
-//             apiUrl = apiUrl + "&rt=8,56";
-//             expect( request.get.calledWith( apiUrl ) ).to.equal( true );
-
-//             done();
-//         } );
-//     } );
-// } );
+describe( "vehiclesById", function () {
+    it( "returns vehicle data", function ( done ) {
+        busTracker.vehiclesById( [ testVehicleId ], function ( err, data ) {
+            if ( err ) {
+                return done( err );
+            }
+            expect( data ).to.have.a.property( "vid" );
+            expect( data ).to.have.a.property( "tmstmp" );
+            expect( data ).to.have.a.property( "lat" );
+            expect( data ).to.have.a.property( "lon" );
+            expect( data ).to.have.a.property( "hdg" );
+            expect( data ).to.have.a.property( "pid" );
+            expect( data ).to.have.a.property( "rt" );
+            expect( data ).to.have.a.property( "des" );
+            expect( data ).to.have.a.property( "pdist" );
+            expect( data ).to.have.a.property( "tablockid" );
+            expect( data ).to.have.a.property( "tatripid" );
+            expect( data ).to.have.a.property( "zone" );
+            done();
+        } );
+    } );
+} );
 
 
-// describe( "routes", function () {
-//     it( "calls the correct CTA URL", function ( done ) {
-//         busTracker.routes( function ( err, data ) {
-//             if ( err ) {
-//                 return done( err );
-//             }
-
-//             var apiUrl = "http://www.ctabustracker.com/bustime/api/v1/getroutes?key=";
-//             apiUrl = apiUrl + apiKey;
-//             expect( request.get.calledWith( apiUrl ) ).to.equal( true );
-
-//             done();
-//         } );
-//     } );
-// } );
+describe( "routes", function () {
+    it( "calls the correct CTA URL", function ( done ) {
+        busTracker.routes( function ( err, data ) {
+            if ( err ) {
+                return done( err );
+            }
+            expect( data ).to.be.an( "array" );
+            expect( data[ 0 ] ).to.have.a.property( "rt" );
+            expect( data[ 0 ] ).to.have.a.property( "rtnm" );
+            expect( data[ 0 ] ).to.have.a.property( "rtclr" );
+            done();
+        } );
+    } );
+} );
 
 
 // describe( "routeDirections", function () {
